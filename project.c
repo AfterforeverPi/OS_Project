bd528dfb9ae362a5e9da4c47905f1c99d3a9f3d2
AfterForeverPi <mircea.pipa-bulz@student.upt.ro>
2024-05-10 21:35:27 +0300

    Add week6 and week7 milestones

--- project.c
+++ project.c
@@ -6,16 +6,28 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
+char outputPath[128] = "\0";
 
-int checkForDirectories(int argc, char** argv){
+int checkArguments(int argc, char** argv){
 
     for(int i = 1; i < argc; i++){
-        struct stat statbuf;
-        lstat(argv[i], &statbuf);
-        if(!S_ISDIR(statbuf.st_mode))
-            return 0;
+        
+        if(strcmp(argv[i], "-o") == 0){
+            if(i == argc-1){
+                perror("Option arguments cannot be at the end of the command line");
+            }
 
+            snprintf(outputPath, sizeof(outputPath), "%s", argv[++i]);
+
+        }
+        else{
+            struct stat statbuf;
+            lstat(argv[i], &statbuf);
+            if(!S_ISDIR(statbuf.st_mode))
+                return 0;
+        }
     }
 
     return 1;
@@ -23,49 +35,100 @@ int checkForDirectories(int argc, char** argv){
 
 }
 
-void dirRead(char* filename){
+void printIndent(int indent) {
+    while (indent--) {
+        printf("|   ");
+    }
+}
 
-    DIR *dir;
+void fillSnapshot(char* snapPath, struct stat statbuf, struct dirent* dp) {
+    int out;
+    char metadata[1000];
 
-    if((dir = opendir(filename)) == NULL){
-        perror("Input directory not found");
+    // Open the file in append mode or create it if it doesn't exist
+    if ((out = open(snapPath, O_WRONLY | O_CREAT | O_TRUNC , 0644)) < 0) {
+        perror("Error at opening the snapshot");
+        return;
     }
 
-    char filepath[512];
+    snprintf(metadata, sizeof(metadata), "Entry: %s\nSize: %ld bytes\nLastModified: %lo\nPermissions: %o\n\n",
+             dp->d_name, statbuf.st_size, statbuf.st_mtime, statbuf.st_mode);
 
-    struct dirent *file;
-    while((file = readdir(dir))){
-
-        if(file->d_name[0] != '.'){
-            struct stat statbuf;
-            snprintf(filepath, sizeof(filepath), "%s/%s", filename, file->d_name);
-            if(stat(filepath, &statbuf) == -1){
-                perror("Error at stat");
-                continue;
-            }
-
-            if(S_ISDIR(statbuf.st_mode)){
-                printf("%s\n  ", file->d_name);
-                dirRead(filepath);
-            }
-            else printf("-%s\n", file->d_name);
-
-
-        }
-
-
-    }
-
-    printf("  ");
-
+    write(out, metadata, strlen(metadata));
+    close(out);
 
 }
+
+
+char *getDirName(char* path){
+    char* dirName = strrchr(path, '/');
+                if (dirName != NULL) {
+                    dirName++; // Skip the '/'
+                } else {
+                    dirName = path; // Use the whole path if no '/'
+                }
+    return dirName;
+
+}
+
+void dirRead(char* basePath, int depth) {
+    DIR *dir;
+    struct dirent *dp;
+    char path[1000], snapPath[1000];
+    struct stat statbuf;
+
+    if ((dir = opendir(basePath)) == NULL) {
+        perror("Cannot open directory");
+        return;
+    }
+
+
+
+    if(strcmp(outputPath, "\0") != 0){
+        snprintf(snapPath, sizeof(snapPath), "%s/%s_Snapshot.txt", outputPath, getDirName(basePath));
+    }
+    else
+        snprintf(snapPath, sizeof(snapPath), "%s/Snapshot.txt", basePath);
+    
+
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") && strcmp(dp->d_name, "Snapshot.txt")!= 0) {
+
+            snprintf(path, sizeof(path), "%s/%s", basePath, dp->d_name);
+            if (stat(path, &statbuf) == -1){
+               continue;
+            }
+
+            
+            fillSnapshot(snapPath, statbuf, dp);
+
+            // Check if it's a directory or file
+            if (S_ISDIR(statbuf.st_mode)) {
+                printIndent(depth);
+                printf("|_ %s\n", dp->d_name);
+                // Recursively call with increased depth
+                dirRead(path, depth + 1);
+            } else {
+                printIndent(depth);
+                printf("|_ %s\n", dp->d_name);
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
 
 void dirParse(char argc, char** argv){
 
     for(int i = 1; i < argc; i++){
-        printf("%s\n  ", argv[i]);
-        dirRead(argv[i]);
+        if(strcmp(argv[i], "-o") == 0){
+            i++;
+            continue;
+
+        }
+        printf("%s\n", argv[i]);
+        dirRead(argv[i], 0);
         putchar('\n');
     }
 
@@ -80,7 +143,7 @@ int main(int argc, char** argv){
         perror("Invalid number of arguments\n");
     }
 
-    if(!checkForDirectories(argc, argv))
+    if(!checkArguments(argc, argv))
         perror("Only directories are allowed as arguments\n");
 
     
